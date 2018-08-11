102f9ea6196bda183ac5ede42fb10dba18e95377
Nelson Neto <nelson.ash@outlook.com>
2018-08-11 12:16:39 -0300

    Controller

--- Projeto.Presentation.UI/App/Controllers/Estoque/estoque-cadastro-controller.js
+++ Projeto.Presentation.UI/App/Controllers/Estoque/estoque-cadastro-controller.js
@@ -1,3 +1,24 @@
 ﻿app.controller('estoque-cadastro-controller',
     function ($scope, $estoqueServices) {
+        $scope.model = {
+            Nome: ''
+        };
+
+        $scope.Cadastrar = function () {
+            $scope.sucesso = '';
+            $scope.erro = '';
+
+            $estoqueServices.cadastrar($scope.model)
+                .then(function (d) {
+
+                    $scope.sucesso = 'Estoque cadastrado com sucesso!';
+                    $scope.model.Nome = '';
+
+                })
+                .catch(function (e) {
+
+                    $scope.erro = 'Ocorreu um erro:' + e.data;
+
+                });
+        };
     });
