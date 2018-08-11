9493a8969c664523c3a455418bcb2f12d45c91bc
Nelson Neto <nelson.ash@outlook.com>
2018-08-11 13:06:45 -0300

    Table

--- Projeto.Presentation.UI/App/Controllers/Estoque/estoque-consulta-controller.js
+++ Projeto.Presentation.UI/App/Controllers/Estoque/estoque-consulta-controller.js
@@ -1,3 +1,18 @@
 ﻿app.controller('estoque-consulta-controller',
     function ($scope, $estoqueServices) {
+
+        $scope.erro = "";
+
+        $estoqueServices.consultar()
+            .then(function (d) {
+
+                $scope.estoqueList = d.data;
+
+            })
+            .catch(function (e) {
+                $scope.erro = "Ocorreu um erro: " + e.data;
+            })
+            .finally(function () {
+                //TODO
+            });
     });
